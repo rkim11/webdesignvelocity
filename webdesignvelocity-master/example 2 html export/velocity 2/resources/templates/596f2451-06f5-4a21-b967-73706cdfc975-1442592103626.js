5ba2daf6d18e7fb26004db651a788c184f14b65c
Ming wei Chan <mcha1524@uni.sydney.edu.au>
2015-11-16 22:17:03 +1100

    hhh

---
+++ webdesignvelocity-master/example 2 html export/velocity 2/resources/templates/596f2451-06f5-4a21-b967-73706cdfc975-1442592103626.js
@@ -0,0 +1,315 @@
+jQuery("#simulation")
+  .on("mouseenter dragenter", ".t-596f2451-06f5-4a21-b967-73706cdfc975 .mouseenter", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#t-Label_3") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_3": {
+                      "attributes": {
+                        "font-size": "10.0pt",
+                        "font-family": "Avenir Next,Arial"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_3 .valign": {
+                      "attributes": {
+                        "vertical-align": "middle",
+                        "text-align": "center"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_3 span": {
+                      "attributes": {
+                        "color": "#FD999A",
+                        "text-align": "center",
+                        "text-decoration": "none",
+                        "font-family": "Avenir Next,Arial",
+                        "font-size": "10.0pt",
+                        "font-style": "normal",
+                        "font-weight": "200"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#t-Label_4") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_4": {
+                      "attributes": {
+                        "font-size": "10.0pt",
+                        "font-family": "Avenir Next,Arial"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_4 .valign": {
+                      "attributes": {
+                        "vertical-align": "middle",
+                        "text-align": "center"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_4 span": {
+                      "attributes": {
+                        "color": "#FD999A",
+                        "text-align": "center",
+                        "text-decoration": "none",
+                        "font-family": "Avenir Next,Arial",
+                        "font-size": "10.0pt",
+                        "font-style": "normal",
+                        "font-weight": "200"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#t-Label_5") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_5": {
+                      "attributes": {
+                        "font-size": "10.0pt",
+                        "font-family": "Avenir Next,Arial"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_5 .valign": {
+                      "attributes": {
+                        "vertical-align": "middle",
+                        "text-align": "center"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_5 span": {
+                      "attributes": {
+                        "color": "#FD999A",
+                        "text-align": "center",
+                        "text-decoration": "none",
+                        "font-family": "Avenir Next,Arial",
+                        "font-size": "10.0pt",
+                        "font-style": "normal",
+                        "font-weight": "200"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#t-Label_6") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_6": {
+                      "attributes": {
+                        "font-size": "10.0pt",
+                        "font-family": "Avenir Next,Arial"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_6 .valign": {
+                      "attributes": {
+                        "vertical-align": "middle",
+                        "text-align": "center"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_6 span": {
+                      "attributes": {
+                        "color": "#FD999A",
+                        "text-align": "center",
+                        "text-decoration": "none",
+                        "font-family": "Avenir Next,Arial",
+                        "font-size": "10.0pt",
+                        "font-style": "normal",
+                        "font-weight": "200"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#t-Label_7") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_7": {
+                      "attributes": {
+                        "font-size": "10.0pt",
+                        "font-family": "Avenir Next,Arial"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_7 .valign": {
+                      "attributes": {
+                        "vertical-align": "middle",
+                        "text-align": "center"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_7 span": {
+                      "attributes": {
+                        "color": "#FD999A",
+                        "text-align": "center",
+                        "text-decoration": "none",
+                        "font-family": "Avenir Next,Arial",
+                        "font-size": "10.0pt",
+                        "font-style": "normal",
+                        "font-weight": "200"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#t-Label_10") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_10": {
+                      "attributes": {
+                        "font-size": "10.0pt",
+                        "font-family": "Avenir Next,Arial"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_10 .valign": {
+                      "attributes": {
+                        "vertical-align": "middle",
+                        "text-align": "center"
+                      }
+                    }
+                  },{
+                    "#t-596f2451-06f5-4a21-b967-73706cdfc975 #t-Label_10 span": {
+                      "attributes": {
+                        "color": "#FD999A",
+                        "text-align": "center",
+                        "text-decoration": "none",
+                        "font-family": "Avenir Next,Arial",
+                        "font-size": "10.0pt",
+                        "font-style": "normal",
+                        "font-weight": "200"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseleave dragleave", ".t-596f2451-06f5-4a21-b967-73706cdfc975 .mouseleave", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#t-Label_3")) {
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#t-Label_4")) {
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#t-Label_5")) {
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#t-Label_6")) {
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#t-Label_7")) {
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#t-Label_10")) {
+      jEvent.undoCases(jFirer);
+    }
+  });
