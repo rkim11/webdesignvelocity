51adf6f18c7af6f51e85fbb9ac9e43905d203429
Ming wei Chan <mcha1524@uni.sydney.edu.au>
2015-11-16 22:22:28 +1100

    hif

--- webdesignvelocity-master/velocity/js/velocity.js
+++ webdesignvelocity-master/velocity/js/velocity.js
@@ -3,65 +3,40 @@
 'use strict';
 
 $(document).ready(function () {
-    
-    
-     var stickyNavTop = $('.top-head').offset().top;
-    
-    var stickyNav = function(){
+
+
+    var stickyNavTop = $('.top-head').offset().top;
+
+    var stickyNav = function () {
         var scrollTop = $(window).scrollTop();
-        
+
         if (scrollTop > stickyNavTop) {
-            $('.top-head').addClass('navbar-fixed1', 1000);   
-        }else{
-            $('.top-head').removeClass('navbar-fixed1', 1000);   
+            $('.top-head').addClass('navbar-fixed1', 1000);
+        } else {
+            $('.top-head').removeClass('navbar-fixed1', 1000);
         }
     };
-    
+
     stickyNav();
-    
-    $(window).scroll(function() {
-        stickyNav(); 
+
+    $(window).scroll(function () {
+        stickyNav();
     });
-    
-    
-    
-    
-//    var stickyNavTop = $('.top-head').offset().top;
-//    
-//    var stickyNav = function(){
-//        var scrollTop = $(window).scrollTop();
-//        
-//        if (scrollTop > stickyNavTop) {
-//            $('.top-head').addClass('navbar-fixed1');   
-//        }else{
-//            $('.top-head').removeClass('navbar-fixed1');   
-//        }
-//    };
-//    
-//    stickyNav();
-//    
-//    $(window).scroll(function() {
-//        stickyNav(); 
-//    });
-//    
-//    
-//    
-    
-    
-    
-    
-    
-//
-//  $(window).scroll(function () {                           
-//      //if you hard code, then use console
-//      //.log to determine when you want the 
-//      //nav bar to stick.  
-//      console.log($(window).scrollTop())
-//    if ($(window).scrollTop() > 50) {
-//      $('#navbar').addClass('navbar-fixed1');
-//    }
-//    if ($(window).scrollTop() < 51) {
-//      $('#navbar').removeClass('navbar-fixed1');
-//    }
-//  });
+
+    //Smoothscroll function
+    $(function () {
+        $('a[href*=#][class*="smoothScroll"]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 180
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
 });
